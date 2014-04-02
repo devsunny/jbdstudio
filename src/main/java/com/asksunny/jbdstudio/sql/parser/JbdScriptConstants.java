368322a085ff7cd4aab8904325102768986d18f8
Developer Sunny <sunnyliu2@gmail.com>
2014-04-01 22:41:01 -0400

    Fix look ahead bug

--- src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptConstants.java
+++ src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptConstants.java
@@ -3,10 +3,10 @@ package com.asksunny.jbdstudio.sql.parser;
 
 public class JbdScriptConstants 
 {
-	public static  int PLAIN_LITERAL_TOKEN = 0;
-	public static int DOUBLE_QUOTE_LITERAL = 1;
-	public static int SINGLE_QUOTE_LITERAL = 2;
+	public static final  int PLAIN_LITERAL_TOKEN = 0;
+	public static final int DOUBLE_QUOTE_LITERAL = 1;
+	public static final int SINGLE_QUOTE_LITERAL = 2;
 	
-	public static int SEMICOLON = 9999;
+	public static final int SEMICOLON = 9999;
 	
 }
--- src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptLexer.java
+++ src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptLexer.java
@@ -3,117 +3,160 @@ package com.asksunny.jbdstudio.sql.parser;
 import java.io.IOException;
 import java.io.Reader;
 
+public class JbdScriptLexer {
 
-
-public class JbdScriptLexer 
-{
-	
-	int readAHead = -1;
-	Reader reader = null;
+	int peek = -1;
 	boolean eof = false;
 	int line = 1;
 	int column = 0;
+	Reader reader = null;
 
 	public JbdScriptLexer(Reader reader) {
 		super();
 		this.reader = reader;
 	}
-	
-	public JbdScriptToken nextToken() throws IOException
-	{
-		
-		if(eof) return null;		
-		boolean r = true;
+
+	public JbdScriptToken nextToken() throws IOException {
+
+		if (eof)
+			return null;
 		StringBuilder buf = new StringBuilder();
-		while(r){				
-			int ci = reader.read();
-			//System.out.println(">>>>" + ci);
+		boolean r = true;
+		while (r) {
+			int ci = peek();
+			// System.out.println(">>>>" + ci);
 			column++;
-			if(ci==-1){
+			if (ci == -1) {
 				reader.close();
-				eof=true;
-				r =false;
-				if(buf.length()==0) return null; else break;					
-			}
-			char c = (char)ci;
-			if(c ==';'){
-				JbdScriptToken t = new JbdScriptToken(";", JbdScriptConstants.SEMICOLON, line, column);
+				eof = true;
 				r = false;
-				return t;					
-			}else if(c =='"' || c =='\''){
+				if (buf.length() == 0)
+					return null;
+				else
+					break;
+			}
+			char c = (char) ci;
+			if (c == ';') {
+				JbdScriptToken t = null;
+				if (buf.length() > 0) {
+					t = new JbdScriptToken(buf.toString(),
+							JbdScriptConstants.PLAIN_LITERAL_TOKEN, line,
+							column);
+				} else {
+					read();
+					t = new JbdScriptToken(buf.toString(),
+							JbdScriptConstants.SEMICOLON, line, column);
+				}
+				r = false;
+				return t;
+			} else if (c == '"' || c == '\'') {
+				read();
 				readTo(c, buf);
 				r = false;
-				int k = (c=='"')?JbdScriptConstants.DOUBLE_QUOTE_LITERAL:JbdScriptConstants.SINGLE_QUOTE_LITERAL;
-				JbdScriptToken t = new JbdScriptToken(buf.toString(), k, line, column);
+				int k = (c == '"') ? JbdScriptConstants.DOUBLE_QUOTE_LITERAL
+						: JbdScriptConstants.SINGLE_QUOTE_LITERAL;
+				JbdScriptToken t = new JbdScriptToken(buf.toString(), k, line,
+						column);
 				return t;
-			}else if(Character.isWhitespace(c)){				
-				if(c=='\n'){
+			} else if (Character.isWhitespace(c)) {
+				read();
+				if (c == '\n') {
 					line++;
-					column=0;
-				}				
-				if(buf.length()==0){
+					column = 0;
+				}
+				if (buf.length() == 0) {
 					continue;
-				}else{
+				} else {
 					r = false;
 					break;
 				}
-			}else{
+			} else {
+				read();
 				buf.append(c);
-			}			
-		}
-		JbdScriptToken t = new JbdScriptToken(buf.toString(), JbdScriptConstants.PLAIN_LITERAL_TOKEN, line, column);		
-		return t;
-		
-	}
-	
-	protected void readTo(char c1, StringBuilder buf) throws IOException
-	{
-		for(;;){
-			int ci = reader.read();
-			column++;
-			if(ci==-1){
-				reader.close();
-				eof=true;				
-				break;					
 			}
-			char c = (char)ci;
-			if(c==c1){
+		}
+		JbdScriptToken t = new JbdScriptToken(buf.toString(),
+				JbdScriptConstants.PLAIN_LITERAL_TOKEN, line, column);
+		return t;
+
+	}
+
+	protected void readTo(char c1, StringBuilder buf) throws IOException {
+		for (;;) {
+			int ci = read();
+			column++;
+			if (ci == -1) {
+				reader.close();
+				eof = true;
 				break;
-			}else if(c=='\\'){
-				char c2 = (char)reader.read();
+			}
+			char c = (char) ci;
+			if (c == c1) {
+				break;
+			} else if (c == '\\') {
+				char c2 = (char) read();
 				column++;
-				switch(c2)
-				{
+				switch (c2) {
 				case 't':
-					buf.append("\t");
+					buf.append("\\t");
 					break;
 				case 'b':
-					buf.append("\b");
+					buf.append("\\b");
 					break;
 				case 'n':
-					buf.append("\n");
+					buf.append("\\n");
 					break;
 				case 'r':
-					buf.append("\r");
+					buf.append("\\r");
 					break;
 				case '\'':
-					buf.append("'");
+					buf.append("\\'");
 					break;
 				case '"':
-					buf.append("\"");
+					buf.append("\\\"");
 					break;
 				case '\\':
-					buf.append("\\");
+					buf.append("\\\\");
 					break;
 				default:
-					throw new IOException(String.format("Unknow character at line %d column %d", line, column));
+					throw new IOException(String.format(
+							"Unknow character at line %d column %d", line,
+							column));
 				}
-			}else{
+			} else {
 				buf.append(c);
 			}
-			
+
 		}
-		
+
 	}
-	
+
+	protected int peek() throws IOException {
+		if (peek == -1 && !eof) {
+			peek = reader.read();
+			if (peek == -1)
+				eof = true;
+		}
+		return peek;
+	}
+
+	protected int read() throws IOException {
+
+		int r = -1;
+		if (peek != -1) {
+			r = peek;
+			peek = reader.read();
+			if (peek == -1)
+				eof = true;
+		} else if (peek == -1 && !eof) {
+			r = reader.read();
+			if (r == -1)
+				eof = true;
+		} else {
+			r = -1;
+		}
+		return r;
+
+	}
+
 }
