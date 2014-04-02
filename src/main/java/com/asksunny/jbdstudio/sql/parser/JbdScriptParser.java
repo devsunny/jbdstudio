fdc0dd684de6952d58f0eaa498bed26162afc70f
Developer Sunny <sunnyliu2@gmail.com>
2014-04-01 22:41:29 -0400

    Fix format

--- src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptParser.java
+++ src/main/java/com/asksunny/jbdstudio/sql/parser/JbdScriptParser.java
@@ -5,44 +5,40 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class JbdScriptParser {
-	
+
 	JbdScriptLexer lexer = null;
-	
-	public JbdScriptParser(String script)
-	{
+
+	public JbdScriptParser(String script) {
 		lexer = new JbdScriptLexer(new StringReader(script));
 	}
-	
-	
-	public String nextStatement()
-	{
+
+	public String nextStatement() {
 		List<JbdScriptToken> tokens = new ArrayList<JbdScriptToken>();
-		
+
 		try {
 			JbdScriptToken t = null;
 			boolean stop = false;
-			do{
+			do {
 				t = lexer.nextToken();
-				if(t==null || t.kind==JbdScriptConstants.SEMICOLON){					
+				if (t == null || t.kind == JbdScriptConstants.SEMICOLON) {
 					stop = true;
 					break;
-				}else{					
+				} else {
 					tokens.add(t);
-				}			
-			}while(!stop);
+				}
+			} while (!stop);
 		} catch (IOException e) {
-			//should never be here;
+			// should never be here;
 			e.printStackTrace();
 		}
 		int size = tokens.size();
-		if(size==0) return null;
+		if (size == 0)
+			return null;
 		StringBuilder buf = new StringBuilder();
-		for(int i=0; i<size; i++){
+		for (int i = 0; i < size; i++) {
 			JbdScriptToken t = tokens.get(i);
-			switch(t.kind)
-			{
+			switch (t.kind) {
 			case JbdScriptConstants.DOUBLE_QUOTE_LITERAL:
 				buf.append('"').append(t.image).append('"');
 				break;
@@ -53,20 +49,20 @@ public class JbdScriptParser {
 				buf.append(tokens.get(i).image);
 				break;
 			}
-			if(i<size-1) buf.append(" ");
+			if (i < size - 1)
+				buf.append(" ");
 		}
 		return buf.toString();
 	}
-	
-	
-	public static void main(String[] args) throws Exception{
-		
+
+	public static void main(String[] args) throws Exception {
+
 		String test = "SELECT *, 'my \\ntext' as \"my column\" FROM test;\n insert into my table; ";
 		JbdScriptParser reader = new JbdScriptParser(test);
 		String stmt = null;
-		while((stmt=reader.nextStatement())!=null){
-			System.out.println(stmt);			
-		}		
+		while ((stmt = reader.nextStatement()) != null) {
+			System.out.println(stmt);
+		}
 	}
-	
+
 }
